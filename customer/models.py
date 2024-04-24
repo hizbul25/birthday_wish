71cc825f9c0388b39b74b1746022b596cca10de7
Hizbul Bahar <hizbul.ku@gmail.com>
2024-04-24 09:08:27 +0600

    create customer model as well migration

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+from common.models import TimeStampedModel
+
 # Create your models here.
+
+
+class Customer(TimeStampedModel):
+    name = models.CharField(max_length=100, blank=False)
+    email = models.EmailField(unique=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
+    dob = models.DateField(db_index=True, blank=False)
+
+    def __str__(self) -> str:
+        return self.name
